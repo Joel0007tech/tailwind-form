2f3038dbc2de3a794691e8b2d48cc1029586bbbc
Joel <ajiolajoel@gmail.com>
2023-04-02 13:56:30 +0100

    update to the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const Form = () => {
   return (
     <div>
-      <form className="h-72 bg-green-950 m-auto w-80 sm:w-60 sm:h-80 sm:truncate">
-        <h3 className="font-bold text-white text-center py-5">
+      <form className="h-72 bg-green-600 m-auto w-80 sm:w-60 sm:h-80 sm:truncate">
+        <h3 className="font-bold text-white text-center py-5 sm:text-black">
           Log in to continue
         </h3>
         <div className="flex flex-col gap-6 py-3 sm:flex-col sm:gap-2 sm:m-auto sm:py-0">
           <div className="flex flex-row m-auto gap-2 sm:flex-col sm:gap-2">
-            <p className=" text-white text-m my-1 font-normal ml-5 sm:text-center">E-mail;</p>
+            <p className=" text-white text-m my-1 font-normal ml-5 sm:text-center sm:text-black">E-mail;</p>
             <input className=" bg-slate-600 text-white h-10 w-45 sm:h-8 sm:bg-white sm:text-black" />
           </div>
           <div className="flex flex-row m-auto gap-2 sm:flex-col sm:gap-2">
-            <p className=" text-white text-m my-1 font-normal sm:text-center">Password;</p>
+            <p className=" text-white text-m my-1 font-normal sm:text-center sm:text-black">Password;</p>
             <input className=" bg-slate-600 text-white h-10 w-45 sm:h-8 sm:bg-white sm:text-black" />
           </div>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <h2 className='font-bold text-center text-white py-8'>FORM PAGE</h2>
+      <h2 className='font-bold text-center text-white py-8 sm:text-green-500'>FORM PAGE</h2>
     </div>
   )
 }
